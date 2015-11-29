08656def2acb5077b7b6f3f0ddc29e166390e5de
Steven <Steven@Steven-PC>
2015-11-29 12:09:25 -0500

    2

---
+++ src/model/Ident.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.List;
+import java.util.regex.*;
+
+import org.antlr.v4.runtime.Token;
+
+import controller.App;
+
+public class Ident {
+	
+	public void identCheck(List<Token> tokens, int identLevel) {
+		int rt = 0;
+		Pattern space = Pattern.compile(" ");
+		Pattern tab = Pattern.compile("\t");
+		for(Token token: tokens){
+			Matcher sp = space.matcher(token.getText());
+			Matcher tb = tab.matcher(token.getText());
+			if(sp.find(0)){
+				rt += spacesCheck(sp, token);
+			}
+			else if(tb.find()){
+				rt += token.getText().length();
+			}
+		}
+		if(rt != identLevel)
+			App.getInstance().setError(tokens.get(0), TipoErrores.IDENTACION);
+	}
+
+	private int spacesCheck(Matcher sp, Token token) {
+		int rt = 0;
+		int spaces = 0;
+		while(sp.find()){
+			spaces ++;
+		}
+		spaces++;
+		if(spaces % 4 == 0)
+			rt += spaces/4;
+		else
+			App.getInstance().setError(token,TipoErrores.ESPACIOS);
+		return rt;
+		
+	}
+}
+	
