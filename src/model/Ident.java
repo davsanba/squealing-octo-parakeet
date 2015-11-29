b211bdfff98a4fa96eaaf1e6ed2cda63be4acda7
David S. Barrera <dsbarrerag@gmail.com>
2015-11-29 10:53:02 -0500

    asf

--- src/model/Ident.java
+++ src/model/Ident.java
@@ -14,20 +14,33 @@ public class Ident {
 		Pattern space = Pattern.compile(" ");
 		Pattern tab = Pattern.compile("\t");
 		for(Token token: tokens){
-			int spaces = 0;
 			Matcher sp = space.matcher(token.getText());
 			Matcher tb = tab.matcher(token.getText());
-			while(sp.find()){
-				spaces ++;
+			if(sp.find(0)){
+				rt += spacesCheck(sp, token);
 			}
-			while(tb.find())
-				rt ++;
-			if(spaces % 4 == 0)
-				rt ++ ;
-			else
-				App.getInstance().setError(token,TipoErrores.ESPACIOS);
+			else if(tb.find()){
+				
+				rt += token.getText().length();
+			}
 		}
+		System.out.println(rt);
 		return rt;
+	}
+
+	private int spacesCheck(Matcher sp, Token token) {
+		int rt = 0;
+		int spaces = 0;
+		while(sp.find()){
+			spaces ++;
+		}
+		spaces++;
+		if(spaces % 4 == 0)
+			rt += spaces/4;
+		else
+			App.getInstance().setError(token,TipoErrores.ESPACIOS);
+		return rt;
+		
 	}
 	
 }
